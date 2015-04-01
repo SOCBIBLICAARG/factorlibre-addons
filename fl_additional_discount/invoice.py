f03635bb7645466ca2309f2245f97c81ddf7cb40
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2015-04-01 15:51:13 -0300

    [FIX] Precision error.

--- fl_additional_discount/invoice.py
+++ fl_additional_discount/invoice.py
@@ -9,12 +9,17 @@ class account_invoice(models.Model):
     @api.one
     @api.depends('invoice_line.price_subtotal', 'tax_line.amount', 'add_disc')
     def _compute_amount(self):
+        precision = self.env['decimal.precision'].precision_get('Account')
         self.amount_untaxed = sum(line.price_subtotal
                                   for line in self.invoice_line)
-        self.add_disc_amt = self.amount_untaxed * self.add_disc/100
+        self.add_disc_amt = round(
+            self.amount_untaxed * self.add_disc/100,
+            precision)
         self.amount_net = self.amount_untaxed - self.add_disc_amt
-        self.amount_tax = sum(line.amount for line in self.tax_line) \
-            * (1 - self.add_disc/100)
+        self.amount_tax = round(
+            sum(line.amount for line in self.tax_line) *
+            (1 - self.add_disc/100),
+            precision)
         self.amount_total = self.amount_net + self.amount_tax
 
     add_disc = fields.Float(
@@ -43,8 +48,11 @@ class account_invoice_line(models.Model):
 
     @api.model
     def move_line_get_item(self, line):
+        precision = self.env['decimal.precision'].precision_get('Account')
         r = super(account_invoice_line, self).move_line_get_item(line)
-        r['price'] = r['price'] * ((100 - line.invoice_id.add_disc)/100.0)
+        r['price'] = round(
+            r['price'] * ((100 - line.invoice_id.add_disc)/100.0),
+            precision)
         return r
 account_invoice_line()
 
